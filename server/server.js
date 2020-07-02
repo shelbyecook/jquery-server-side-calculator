b97b32dd889dc72f3599685c10ca0f0c13a041bc
Shelby Cook <shelby.elle.cook@gmail.com>
2020-07-02 16:38:10 -0500

    set up the bones of client.js file

--- server/server.js
+++ server/server.js
@@ -4,9 +4,11 @@ const app = express();
 const PORT = 5000;
 
 // This must be added before GET & POST routes.
-app.use(express.static('server/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+//This serves the static files.
+app.use(express.static('server/public'));
 
 app.listen(5000, () => {
   console.log(`Listening on port 5000!`);
