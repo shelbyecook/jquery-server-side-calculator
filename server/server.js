a33b4941784f7166f57c509bb1ad7dcb3a05f3cb
Shelby Cook <shelby.elle.cook@gmail.com>
2020-07-04 15:09:05 -0500

    created get history server destination

--- server/server.js
+++ server/server.js
@@ -7,6 +7,21 @@ const PORT = 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+//data structure
+const history = [
+  // {
+  //   mathVal1: '',
+  //   mathVal2: '',
+  //   mathOperator: '',
+  //   solution: ''
+  // }
+];
+
+//GET of equation history tested on Postman
+app.get('/api/equation-history', (req, res) => {
+  res.send(history);
+});
+
 //This serves the static files.
 app.use(express.static('server/public'));
 
